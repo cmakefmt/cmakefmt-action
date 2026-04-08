edc53b3e2b6e07ba885d664658bf757834159a7b
Puneet Matharu <puneet.matharu@hotmail.co.uk>
2026-04-08 23:48:00 +0100

    Use plain JS for jest config so ts-node is not required

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
-import type { Config } from "jest";
-
-const config: Config = {
+/** @type {import('jest').Config} */
+module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["<rootDir>/__tests__/**/*.test.ts"],
@@ -12,5 +11,3 @@ const config: Config = {
     ],
   },
 };
-
-export default config;
